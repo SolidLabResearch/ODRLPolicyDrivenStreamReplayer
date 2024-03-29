059d33c02a1e8ded35484d97fea8c9b225fd1282
Kushagra Singh Bisen <sendtokush@icloud.com>
2024-03-29 17:21:03 +0100

    added the initial version

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# Name of the package
+# KISS Solid Stream Replayer
 
-# Usage
+A simpler sensor data replayer to the inbox of an LDES in LDP or any LDP container for that matter.
 
-# License
+## License
+
+This code is copyrighted by [Ghent University - imec](https://www.ugent.be/ea/idlab/en) and released under the [MIT Licence](./LICENCE) 
+
+## Contact
+
+For any questions, please contact [Kush](mailto:kushagrasingh.bisen@ugent.be) or create an issue in the repository.
--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,15 @@ async function main() {
     let store = await load_dataset('/home/kush/Code/feather-RDF-mapper/data/rdfData/skt/skt-2min.nt');
     let sorted_observation_subjects = await sort_observation_store(store);
     const ldp_communication = new LDPCommunication();
-    publish_one_observation(sorted_observation_subjects, store, 'http://localhost:3000/aggregation_pod/skt/1711614671461/', ldp_communication);
+    const frequency = 4;
+    if (observation_pointer < sorted_observation_subjects.length) {
+        setInterval(() => {
+            publish_one_observation(sorted_observation_subjects, store, 'http://localhost:3000/aggregation_pod/skt/1711614671461/', ldp_communication);
+        }, 1000 / frequency);
+    }
+    else {
+        console.log('No observations to replay');
+    }    
 }
 
 async function sort_observation_store(store: any) {
@@ -34,12 +42,19 @@ async function publish_one_observation(sorted_observation_subjects: string[], st
     console.log(`Replaying one observation at the pointer ${observation_pointer}`);
     const observation = JSON.stringify(sorted_observation_subjects[observation_pointer]);
     const observation_object = JSON.parse(observation);
+    store.removeQuads(store.getQuads(namedNode(observation_object.subject.value), namedNode('https://saref.etsi.org/core/hasTimestamp'), null, null));
+    const time_now = new Date().toISOString();
+    store.addQuad(namedNode(observation_object.subject.value), namedNode('https://saref.etsi.org/core/hasTimestamp'), literal(time_now));
+    let timestamp = store.getObjects(namedNode(observation_object.subject.value), namedNode('https://saref.etsi.org/core/hasTimestamp', null));
     const store_observation = new N3.Store(store.getQuads(namedNode(observation_object.subject.value), null, null, null))
     let store_observation_string = storeToString(store_observation);
     await ldp_communication.post(location_to_publish, store_observation_string).then((response) => {
         console.log(response.statusText);
     });
     observation_pointer++;
+    if (observation_pointer === sorted_observation_subjects.length) {
+        console.log('All observations replayed');
+    }
 }
 async function load_dataset(dataset_location: string) {
     return new Promise((resolve, reject) => {
