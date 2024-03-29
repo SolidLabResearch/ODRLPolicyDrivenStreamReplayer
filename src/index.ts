d160971878b63ac69728540a235bec9c8db07d5d
Kushagra Singh Bisen <sendtokush@icloud.com>
2024-03-29 16:49:30 +0100

    Add dependencies and refactor code for publishing observations

--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,124 @@
+import { LDP, LDPCommunication, storeToString } from '@treecg/versionawareldesinldp';
 import * as fs from 'fs';
 const N3 = require('n3');
-const file_location = '/home/kush/Code/feather-RDF-mapper/data/rdfData/skt/skt-2min.nt';
-const store = new N3.Store();
-const stream_parser = new N3.StreamParser();
+import { Quad } from 'n3';
 const stream = require('stream');
-import axios from 'axios';
+const { DataFactory } = N3;
+export type Resource = Quad[];
+let observation_pointer = 0;
 
-async function replay() {
-  const observation = `
-  <https://dahcc.idlab.ugent.be/Protego/_participant1/obs0> <http://rdfs.org/ns/void#inDataset> <https://dahcc.idlab.ugent.be/Protego/_participant1> . <https://dahcc.idlab.ugent.be/Protego/_participant1/obs0> <https://saref.etsi.org/core/measurementMadeBy> <https://dahcc.idlab.ugent.be/Homelab/SensorsAndActuators/E4.A03846.Thermopile> . <https://dahcc.idlab.ugent.be/Protego/_participant1/obs0> <http://purl.org/dc/terms/isVersionOf> <https://saref.etsi.org/core/Measurement> . <https://dahcc.idlab.ugent.be/Protego/_participant1/obs0> <https://saref.etsi.org/core/relatesToProperty> <https://dahcc.idlab.ugent.be/Homelab/SensorsAndActuators/wearable.skt> . <https://dahcc.idlab.ugent.be/Protego/_participant1/obs0> <https://saref.etsi.org/core/hasTimestamp> "2024-03-21T08:48:24.2460Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> . <https://dahcc.idlab.ugent.be/Protego/_participant1/obs0> <https://saref.etsi.org/core/hasValue> "32.69"^^<http://www.w3.org/2001/XMLSchema#float> .
-  `
-  const frequency = 4;
-  const replay_seconds = 120;
-  await post_observation(observation, frequency, replay_seconds);
+const { namedNode, literal, defaultGraph, quad } = DataFactory;
+
+async function main() {
+    let store = await load_dataset('/home/kush/Code/feather-RDF-mapper/data/rdfData/skt/skt-2min.nt');
+    let sorted_observation_subjects = await sort_observation_store(store);
+    const ldp_communication = new LDPCommunication();
+    publish_one_observation(sorted_observation_subjects, store, 'http://localhost:3000/aggregation_pod/skt/1711614671461/', ldp_communication);
 }
 
-async function sort() {
-}
-
-async function post_observation(observation: string, frequency: number, replay_seconds: number) {
-  let count = 0;
-  const start_time = new Date().getTime();
-  setInterval(async () => {
-    const response = await axios.post('http://n061-14a.wall2.ilabt.iminds.be:3000/participant6/skt/1711570652135/', observation, {
-      headers: {
-        'Content-Type': 'text/turtle'
-      }
-    });
-    count++;
-    console.log(response.status);
-    console.log('Posted: ', count);
-  }, 1000 / frequency);
-  if (count >= replay_seconds * frequency) {
-    console.log('Done');  
-    const end_time = new Date().getTime();
-    console.log('Time taken: ', end_time - start_time);
-    process.exit(0);
-  }
-}
-
-function stream_consumer() {
-  const writer = stream.Writable({ objectMode: true });
-  writer._write = (quad: any, encoding: any, done: () => void) => {
-    // console.log(quad);
-    store.add(quad);
-    done();
-
-  };
-  return writer;
-}
-
-function mergeSort(list: string[]): string[] {
-  if (list.length <= 1) return list;
-  let mid = Math.floor(list.length / 2);
-  let left: string[] = mergeSort(list.slice(0, mid));
-  let right: string[] = mergeSort(list.slice(mid));
-  return merge(left, right);
-}
-
-
-function merge(list1: string[], list2: string[]): string[] {
-  let merged: string[] = [],
-    i: number = 0,
-    j: number = 0;
-  while (i < list1.length && j < list2.length) {
-    let timestamp1 = store.getObjects(N3.namedNode(list1[i]), N3.namedNode('https://saref.etsi.org/core/hasTimestamp'), null)[0].value;
-    let timestamp2 = store.getObjects(N3.namedNode(list2[j]), N3.namedNode('https://saref.etsi.org/core/hasTimestamp'), null)[0].value;
-
-    if (timestamp1 < timestamp2) {
-      merged.push(list1[i]);
-      i++;
-    } else {
-      merged.push(list2[j]);
-      j++;
+async function sort_observation_store(store: any) {
+    const temporary_array = [];
+    for (const quad of store.match(null, 'https://saref.etsi.org/core/measurementMadeBy', null)) {
+        temporary_array.push(quad);
     }
-  }
-  while (i < list1.length) {
-    merged.push(list1[i]);
-    i++;
-  }
-  while (j < list2.length) {
-    merged.push(list2[j]);
-    j++;
-  }
-  return merged;
+    const sorted_observation_array = merge_sort(temporary_array, store);
+    const reversed_sorted_observation_array = sorted_observation_array.reverse();
+    const sorted_observation_subjects = new Array<string>();
+    reversed_sorted_observation_array.forEach((quad) => {
+        sorted_observation_subjects.push(quad);
+    });
+    return sorted_observation_subjects;
 }
 
-replay();
+async function publish_one_observation(sorted_observation_subjects: string[], store: any, location_to_publish: string, ldp_communication: LDPCommunication) {
+    console.log(`Replaying one observation at the pointer ${observation_pointer}`);
+    const observation = JSON.stringify(sorted_observation_subjects[observation_pointer]);
+    const observation_object = JSON.parse(observation);
+    const store_observation = new N3.Store(store.getQuads(namedNode(observation_object.subject.value), null, null, null))
+    let store_observation_string = storeToString(store_observation);
+    await ldp_communication.post(location_to_publish, store_observation_string).then((response) => {
+        console.log(response.statusText);
+    });
+    observation_pointer++;
+}
+async function load_dataset(dataset_location: string) {
+    return new Promise((resolve, reject) => {
+        const stream_parser = new N3.StreamParser();
+        const rdf_stream = fs.createReadStream(dataset_location);
+        rdf_stream.pipe(stream_parser);
+        const store = new N3.Store();
+        stream_parser.pipe(stream_consumer(store));
+        stream_parser.on('error', reject);
+        stream_parser.on('end', () => {
+            resolve(store);
+        });
+    });
+}
+
+function stream_consumer(store: any) {
+    const writer = stream.Writable({ objectMode: true });
+    writer._write = function (triple: any, encoding: any, done: () => void) {
+        store.add(triple);
+        done();
+    }
+    return writer;
+}
+
+function number_of_triples(store: any) {
+    return store.size;
+}
+
+function number_of_observations(store: any) {
+    let count = 0;
+    for (const quad of store.match(null, namedNode('https://saref.etsi.org/core/measurementMadeBy', null))) {
+        count++;
+    }
+    return count;
+}
+
+function merge_sort(array: string[], store: any): string[] {
+    if (array.length <= 1) {
+        return array;
+    }
+    let middle = Math.floor(array.length / 2);
+    let left: string[] = merge_sort(array.slice(0, middle), store);
+    let right: string[] = merge_sort(array.slice(middle), store);
+    return merge(left, right, store);
+}
+
+function merge(array_one: string[], array_two: string[], store: any): string[] {
+    let merged: string[] = [];
+    let i: number = 0;
+    let j: number = 0;
+
+    while (i < array_one.length && j < array_two.length) {
+
+        let timestamp_one = store.getObjects(namedNode(array_one[i]).id.subject.id, namedNode('https://saref.etsi.org/core/hasTimestamp', null));
+        let timestamp_two = store.getObjects(namedNode(array_two[j]).id.subject.id, namedNode('https://saref.etsi.org/core/hasTimestamp', null));
+
+        if (timestamp_one > timestamp_two) {
+            merged.push(array_one[i]);
+            i++;
+        } else {
+            merged.push(array_two[j]);
+            j++;
+        }
+    }
+
+    while (i < array_one.length) {
+        merged.push(array_one[i]);
+        i++;
+    }
+
+    while (j < array_two.length) {
+        merged.push(array_two[j]);
+        j++;
+    }
+
+    return merged;
+}
+
+
+main();
+
+
